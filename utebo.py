4effa3c02c8b61f2dbb417b4302d6defa7f3d2a7
José Luan Lima da Costa <joseluan620@gmail.com>
2022-03-06 13:33:17 -0300

    Reogarnizando repositorio

--- utebo.py
+++ utebo.py
@@ -1,7 +1,25 @@
 import pygame
 from math import ceil
 
-# Feito por Magnus, Rudigus e Caius e Luan
+## Feito por Magnus, Rudigus e Caius e Luan
+
+# MANUAL #
+# Jogador laranja:
+# A -> movimenta para trás
+# D -> movimenta para frente
+# S -> movimenta para baixo
+# W -> movimenta para cima
+# G -> turbo
+
+# Jogador azul:
+# ArrowLeft   -> movimenta para trás
+# ArrowRight  -> movimenta para frente
+# ArrowDown   -> movimenta para baixo
+# ArrowUp     -> movimenta para cima
+# L           -> turbo
+
+# F1  -> Reinicia o jogo
+# ESC -> Fecha o jogo
 
 placarJ1 = 0
 placarJ2 = 0
